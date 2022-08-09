b87d60530a9aa6239e79f895501c564a4b491acf
punarjeewarashmitha20011016 <punarjeewarashmitha1@gmail.com>
2022-08-09 20:53:02 +0530

    content updated

--- frontend/src/common/CommonComponent.jsx
+++ frontend/src/common/CommonComponent.jsx
@@ -8,7 +8,7 @@ export const PageHeader = ({label, pageTitle}) => {
     return (
         <Grid container >
             <Grid item xs = {12} sm = {12} >
-                <Typography variant = 'body2' className = {classes.pageTitle} > {label} < /Typography>
+                <Typography variant = 'body2' className = {classes.pageTitle} > {label} </Typography>
                 {/*<Typography variant='h5' className={classes.pageSubTitle}> {pageTitle} </Typography>*/}
             </Grid>
         </Grid>
